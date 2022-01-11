de44530272d645117a7517d3edf82bd817c22f6f
Blaize Liebenberg <Blaize.Liebenberg@rapiddeploy.com>
2022-01-11 11:14:39 +0200

    tried to build some test but they not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 import Container from 'react-bootstrap/Container';
-
 import Header from './components/Header';
 import Devs from './routes/Devs';
 import Repos from './routes/Repos';
+
+const Footer = styled.footer`
+  display: flex;
+`;
+
+export const LocationDisplay = () => {
+  const location = useLocation();
+
+  return <div data-testid='location-display'>{location.pathname}</div>;
+};
 
 const App = () => (
   <Container fluid>
     <Header />
     <Container>
       <Routes>
+        <Route exact path='/' element={<Repos />} />
         <Route path='/developers' element={<Devs />} />
-        <Route path='/' element={<Repos />} />
       </Routes>
     </Container>
+    <Footer>
+      Ⓒ Blaize Liebenberg - <LocationDisplay />
+    </Footer>
   </Container>
 );
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { createMemoryHistory } from 'history'
+import React from 'react'
+import { Router } from 'react-router-dom'
+import App, { LocationDisplay } from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('full app rendering/navigating', () => {
+  const history = createMemoryHistory()
+  render(
+    <Router history={history}>
+      <App />
+    </Router>,
+  )
+  // verify page content for expected route
+  expect(screen.getByText(/Trending Repositories/i)).toBeInTheDocument()
+
+  const leftClick = {button: 0}
+  userEvent.click(screen.getByText(/Developers/i), leftClick)
+
+  // check that the content changed to the new page
+  expect(screen.getByText(/Trending Developers/i)).toBeInTheDocument()
+})
+
+test('rendering a component that uses useLocation', () => {
+  const history = createMemoryHistory()
+  const route = '/developers'
+  history.push(route)
+  render(
+    <Router history={history}>
+      <LocationDisplay />
+    </Router>,
+  )
+
+  expect(screen.getByTestId('location-display')).toHaveTextContent(route)
+})
